44a21e747a68674e3ff1f8b40e7372e244c6b78f
ZJK <JKMan0512@163.com>
2019-10-06 08:25:42 +0800

    第二次

--- springBootDemo/src/main/java/com/beijingwanjia/SpringBootTest.java
+++ springBootDemo/src/main/java/com/beijingwanjia/SpringBootTest.java
@@ -3,5 +3,6 @@ package com.beijingwanjia;
 public class SpringBootTest {
     public static void main(String[] args) {
         System.out.println("自律+执行=成功");
+        System.out.println("努力");
     }
 }
